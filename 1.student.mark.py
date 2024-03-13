4e6a013b3bf5aaa43d18ff456bb0af3447803bdc
Tran Duc Huy <huyhayphet123@gmail.com>
2024-03-13 17:08:44 +0700

    up

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,116 +1,120 @@
 import re
 
-students = []
-courses = []
-marks = {}
+list_student = []
+list_course = []
+dict_mark = {}
+
 def DoB_format(date):
     date_pattern = r"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}$"  # MM/DD/YYYY format
     return re.match(date_pattern, date) is not None
-
-def input_number_students():
+    
+def number_student():
     while True:
-        number_student_ver1 = int(input("Enter the number of student in a class: "))
-        if number_student_ver1 < 0:
-            print("Number of student must be a positive number")
-        elif number_student_ver1 == 0:
-            print("Number of student must be exist")
-        else:
-            return number_student_ver1
-        
-def input_student_info():
-    number_student_ver2 = input_number_students()
-    for i in range(1, number_student_ver2 + 1):
-        student_id = input(f"Enter student ID for student {i}: ")
-        student_name = input(f"Enter student name for student {i}: ")
-        while True:
-            student_DoB = input(f"Enter student DoB (Date of Birth) for student {i} in MM/DD/YYYY format: ")
-            if not DoB_format(student_DoB):
-                print("Wrong format. Please enter the DoB in MM/DD/YYYY format.")
+        try:
+            number_student_ver1 = int(input("Enter the number of students: "))
+            if number_student_ver1 <= 0:
+                print("Number of students must be positive.")
             else:
-                students.append({"ID": student_id, "Name": student_name, "DoB": student_DoB})
-                break 
+                return number_student_ver1
+        except ValueError:
+            print("Please enter a valid integer.")
 
-def input_course_number():
+def info_students():
+    number_students = number_student()
+    for i in range(1,number_students + 1):
+        id_student = input(f"Enter student {i} id: ")
+        name_student = input(f"Enter student {i} name: ")
+        while True:
+            try:
+                DoB_student = input("Enter DoB, follow format: MM/DD/YYYY: ")
+                if not DoB_format(DoB_student):
+                    print("Please enter in the format: MM/DD/YYYY")
+                else:
+                    list_student.append({'id': id_student, 'name': name_student, 'DoB': DoB_student})
+                    break
+            except ValueError:
+                print("Please try again")
+
+def number_course():
     while True:
-        number_course_ver1 = int(input("Enter the number of course: "))
-        if number_course_ver1 < 0:
-            print("Number of course must be a positive number")
-        elif number_course_ver1 == 0:
-            print("Number of course must be exist")
-        else:
-            return number_course_ver1
+        try:
+            number_courses = int(input("Enter the number of courses: "))
+            if number_courses <= 0:
+                print("Number of courses must be positive")
+            else:
+                return number_courses
+        except ValueError:
+            print("Please try again")
 
-def input_course_info():
-    number_course_ver2 = input_course_number()
-    for i in range(1, number_course_ver2 + 1):
-        course_id = input(f"Enter course id for course {i}: ")
-        course_name = input(f"Enter course name for course {i}: ")
-        courses.append({"ID": course_id, "Name": course_name})
+def info_courses():
+    infor_course = number_course()
+    for i in range(1, infor_course + 1):
+        id_course = input(f"Enter the id of course {i}: ")
+        name_course = input(f"Enter the name of course {i}: ")
+        list_course.append({"id": id_course, "name": name_course})
 
-def select_course_and_input_marks():
+def select_and_input_marks_in_course():
     while True:
-        course_id = input("Enter the ID of course to input marks (or 'q' to quit): ")
-        if course_id == 'q':
-            break
-        elif course_id not in [i['ID'] for i in courses]:
-            print(f"The course with id {course_id} does not exist")
-        else:
-            marks[course_id] = []
-            for student in students:
-                while True:
-                    mark = float(input(f"Enter mark for student {student['Name']} (ID: {student['ID']}): "))
-                    if 0 <= mark <= 20:
-                        marks[course_id].append((student['ID'], mark))
-                        break
-                    else:
-                        print("Invalid mark. Please enter a mark between 0 and 20.")
+        try:
+            check_course_id = input("Enter the course id of course to input marks or 'q' to quit: ")
+            if check_course_id == 'q':
+                break
+            elif check_course_id not in [i['id']for i in list_course]:
+                print(f"The course with id {check_course_id} does not exist")
+            else:
+                dict_mark[check_course_id] = []
+                for student in list_student:
+                    while True:
+                        mark = float(input(f"Enter mark for student: {student['name']}, ID: {student['id']} "))
+                        if 0 <= mark <= 20:
+                            dict_mark[check_course_id].append((student['id'], mark))  
+                            break
+                        else:
+                            print("Invalid mark. Please enter a mark between 0 and 20.")
+        except ValueError:
+            print("Please enter mark between 0 and 20")
+
 
 def list_courses():
-    for course in courses:
-        print(f"ID: {course['ID']}, Name: {course['Name']}")
+    for i in list_course:
+        print(f"ID: {i['id']}, Name: {i['name']}")
 
 def list_students():
-    for student in students:
-        print(f"ID: {student['ID']}, Name: {student['Name']}, DoB: {student['DoB']}")
+    for i in list_student:
+        print(f"ID: {i['id']}, Name: {i['name']}")
 
-def show_student_marks_for_course():
+def Show_student_marks_for_course():
     course_id = input("Enter the course ID to show marks: ")
-    if course_id in marks:
-        for student_id, mark in marks[course_id]:
-            student_name = next(student['Name'] for student in students if student['ID'] == student_id)
+    if course_id in dict_mark:
+        for student_id, mark in dict_mark[course_id]:
+            student_name = next(student['name'] for student in list_student if student['id'] == student_id)
             print(f"Student: {student_name}, Mark: {mark}")
     else:
         print("No marks recorded for this course.")
-
-def main():
+            
+def main(): 
     while True:
-        print("\n1. Input student information")
-        print("2. Input course information")
-        print("3. Input marks for a course")
-        print("4. List courses")
-        print("5. List students")
-        print("6. Show student marks for a course")
-        print("7. Exit")
-
-        choice = input("\nSelect your choice: ")
-        
+        print("\n1. Input information of student")
+        print("2. Input information of course")
+        print("3. Select course and input mark for student")
+        print("4. List course")
+        print("5. List student")
+        print("6. Show student matks for a given course")
+        print("Other. Exit")
+        choice = input("Enter your choice: ")
         if choice == '1':
-            input_student_info()
-        elif choice == '2':
-            input_course_info()
+            info_students()
+        elif choice =='2':
+            info_courses()
         elif choice == '3':
-            select_course_and_input_marks()
+            select_and_input_marks_in_course()
         elif choice == '4':
             list_courses()
         elif choice == '5':
             list_students()
         elif choice == '6':
-            show_student_marks_for_course()
-        elif choice == '7':
-            break
+            Show_student_marks_for_course()
         else:
-            print("Invalid choice. Please choose again.")
-
+            break
 if __name__ == "__main__":
     main()
-    
