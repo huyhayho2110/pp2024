f14d9282e3894566e83dd439e92ee3a0096da02c
Tran Duc Huy <huyhayphet123@gmail.com>
2024-03-14 16:32:33 +0700

    new update

--- 2.student.mark.oop.py
+++ 2.student.mark.oop.py
@@ -23,26 +23,53 @@ class MarkSheet:
     def add_course(self, course):
         self.courses.append(course)
 
+    def add_marks(self, course_id, student_id, mark):
+        if course_id not in self.marks:
+            self.marks[course_id] = []
+        self.marks[course_id].append((student_id, mark))
+
+    def show_marks_for_course(self, course_id):
+        if course_id in self.marks:
+            for student_id, mark in self.marks[course_id]:
+                student_name = next(student.name for student in self.students if student.id == student_id)
+                print(f"Student: {student_name}, Mark: {mark}")
+        else:
+            print("No marks recorded for this course.")
+
+    def input_students_info(self):
+        number_students = self._input_number("students")
+        for i in range(1, number_students + 1):
+            id_student = input(f"Enter student {i} id: ")
+            name_student = input(f"Enter student {i} name: ")
+            dob_student = self._input_dob()
+            self.add_student(Student(id_student, name_student, dob_student))
+
+    def input_courses_info(self):
+        number_courses = self._input_number("courses")
+        for i in range(1, number_courses + 1):
+            id_course = input(f"Enter the id of course {i}: ")
+            name_course = input(f"Enter the name of course {i}: ")
+            self.add_course(Course(id_course, name_course))
+
     def input_marks(self):
         while True:
-            try:
-                check_course_id = input("Enter the course id of course to input marks or 'q' to quit: ")
-                if check_course_id == 'q':
-                    break
-                elif check_course_id not in [course.id for course in self.courses]:
-                    print(f"The course with id {check_course_id} does not exist")
-                else:
-                    self.marks[check_course_id] = []
-                    for student in self.students:
-                        while True:
+            check_course_id = input("Enter the course id of course to input marks or 'q' to quit: ")
+            if check_course_id == 'q':
+                break
+            elif check_course_id not in [course.id for course in self.courses]:
+                print(f"The course with id {check_course_id} does not exist")
+            else:
+                for student in self.students:
+                    while True:
+                        try:
                             mark = float(input(f"Enter mark for student: {student.name}, ID: {student.id} "))
                             if 0 <= mark <= 20:
-                                self.marks[check_course_id].append((student.id, mark))
+                                self.add_marks(check_course_id, student.id, mark)
                                 break
                             else:
                                 print("Invalid mark. Please enter a mark between 0 and 20.")
-            except ValueError:
-                print("Please enter mark between 0 and 20")
+                        except ValueError:
+                            print("Please enter a valid mark.")
 
     def list_courses(self):
         for course in self.courses:
@@ -52,56 +79,51 @@ class MarkSheet:
         for student in self.students:
             print(f"ID: {student.id}, Name: {student.name}")
 
-    def show_student_marks_for_course(self):
-        course_id = input("Enter the course ID to show marks: ")
-        if course_id in self.marks:
-            for student_id, mark in self.marks[course_id]:
-                student_name = next(student.name for student in self.students if student.id == student_id)
-                print(f"Student: {student_name}, Mark: {mark}")
-        else:
-            print("No marks recorded for this course.")
+    def _input_number(self, item):
+        while True:
+            try:
+                number = int(input(f"Enter the number of {item}: "))
+                if number <= 0:
+                    print(f"Number of {item} must be positive.")
+                else:
+                    return number
+            except ValueError:
+                print("Please enter a valid integer.")
 
-def DoB_format(date):
-    date_pattern = r"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}$"  # MM/DD/YYYY format
-    return re.match(date_pattern, date) is not None
-
-def main():
-    mark_sheet = MarkSheet()
-    
-    while True:
-        print("\n1. Input information of student")
-        print("2. Input information of course")
-        print("3. Select course and input mark for student")
-        print("4. List course")
-        print("5. List student")
-        print("6. Show student marks for a given course")
-        print("Other. Exit")
-        choice = input("Enter your choice: ")
-        
-        if choice == '1':
-            id_student = input("Enter student ID: ")
-            name_student = input("Enter student name: ")
-            dob_student = input("Enter student date of birth (MM/DD/YYYY): ")
-            if DoB_format(dob_student):
-                student = Student(id_student, name_student, dob_student)
-                mark_sheet.add_student(student)
+    def _input_dob(self):
+        while True:
+            dob = input("Enter DoB, follow format: MM/DD/YYYY: ")
+            if re.match(r"^(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])/\d{4}$", dob):
+                return dob
             else:
-                print("Please enter the date of birth in MM/DD/YYYY format")
-        elif choice == '2':
-            id_course = input("Enter course ID: ")
-            name_course = input("Enter course name: ")
-            course = Course(id_course, name_course)
-            mark_sheet.add_course(course)
-        elif choice == '3':
-            mark_sheet.input_marks()
-        elif choice == '4':
-            mark_sheet.list_courses()
-        elif choice == '5':
-            mark_sheet.list_students()
-        elif choice == '6':
-            mark_sheet.show_student_marks_for_course()
-        else:
-            break
+                print("Please enter in the format: MM/DD/YYYY")
+
+    def menu(self):
+        while True:
+            print("\n1. Input information of student")
+            print("2. Input information of course")
+            print("3. Select course and input mark for student")
+            print("4. List course")
+            print("5. List student")
+            print("6. Show student marks for a given course")
+            print("Other. Exit")
+            choice = input("Enter your choice: ")
+            if choice == '1':
+                self.input_students_info()
+            elif choice == '2':
+                self.input_courses_info()
+            elif choice == '3':
+                self.input_marks()
+            elif choice == '4':
+                self.list_courses()
+            elif choice == '5':
+                self.list_students()
+            elif choice == '6':
+                course_id = input("Enter the course ID to show marks: ")
+                self.show_marks_for_course(course_id)
+            else:
+                break
 
 if __name__ == "__main__":
-    main()
+    mark_sheet = MarkSheet()
+    mark_sheet.menu()
